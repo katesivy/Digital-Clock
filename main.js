72d11964cb448b514eca55a95a5bbcee59b73339
katesivy <58646769+katesivy@users.noreply.github.com>
2020-02-21 14:43:19 -0500

    commits

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 
 
-
 function currentTime() {
   let d = new Date ();
   let h = d.getHours();
   let m = d.getMinutes();
   let s = d.getSeconds();
-  return `${h} : ${m} : ${s}`;
-}
+  var hh = twoDigits(h);
+  var mm = twoDigits(m);
+  var ss = twoDigits(s);
+  var ampm = 'AM';
+  if (h >= 12) {
+    ampm = 'PM';
+  }
+  return `${hh} : ${mm} : ${ss} ${ampm}`;
+} 
 
-function updateTime() {
-const y = currentTime();
-const x = document.getElementById('clock');
-x.textContent = y;
+ function updateTime() {
+    const y = currentTime();
+    const x = document.getElementById('clock');
+    x.textContent = y;
 }
 
 setInterval(updateTime, 1000);
+
+function twoDigits(n) {
+  if (n < 10) {
+  n = '0' + n;
+  return n;
+  } else {
+    return n;
+  }
+}
